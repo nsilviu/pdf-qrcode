bd86d2f32ce35b483b365dc59c2ebe64b11be671
Silviu Nistorescu <nistorescu.silviu@gmail.com>
2024-10-22 13:59:46 +0300

    updated consent v2

--- app/constants.js
+++ app/constants.js
@@ -24,28 +24,28 @@ export const brands = [
 export const sessions = [
   {
     id: 1,
-    name: "14 Octombrie 2024 - 20 Octombrie 2024",
+    name: "21 Octombrie 2024 - 27 Octombrie 2024",
     cars: [
       { image: "/cars/a3.png", model: "Audi A3" },
       { image: "/cars/q8.png", model: "Audi Q8" },
     ],
   },
-  {
-    id: 2,
-    name: "21 Octombrie 2024 - 27 Octombrie 2024",
-    cars: [
-      { image: "/cars/a3.png", model: "Audi A3" },
-      { image: "/cars/q3.png", model: "Audi Q3" },
-    ],
-  },
-  {
-    id: 3,
-    name: "28 Octombrie 2024 - 3 Noiembrie 2024",
-    cars: [
-      { image: "/cars/a3.png", model: "Audi A3" },
-      { image: "/cars/q3sb.png", model: "Audi Q5 Sportback" },
-    ],
-  },
+  // {
+  //   id: 2,
+  //   name: "21 Octombrie 2024 - 27 Octombrie 2024",
+  //   cars: [
+  //     { image: "/cars/a3.png", model: "Audi A3" },
+  //     { image: "/cars/q3.png", model: "Audi Q3" },
+  //   ],
+  // },
+  // {
+  //   id: 3,
+  //   name: "28 Octombrie 2024 - 3 Noiembrie 2024",
+  //   cars: [
+  //     { image: "/cars/a3.png", model: "Audi A3" },
+  //     { image: "/cars/q3sb.png", model: "Audi Q5 Sportback" },
+  //   ],
+  // },
 ];
 
 export const dealerships = [
--- app/layout.js
+++ app/layout.js
@@ -13,45 +13,15 @@ export default function RootLayout({ children }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-NMHG5MNP');
-            `,
-          }}
-        />
-
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-F43JZMZBYB"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-F43JZMZBYB');
-            `,
-          }}
-        />
-
-        {/* Facebook Pixel */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1322842665014293');
-              fbq('track', 'PageView');
             `,
           }}
         />
@@ -65,16 +35,6 @@ export default function RootLayout({ children }) {
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
-
-        {/* Facebook Pixel (noscript) */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1322842665014293&ev=PageView&noscript=1"
-          />
         </noscript>
 
         {children}
